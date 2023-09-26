f23787fd1ee947cc8663a646a78d13a155cf1d14
AkhmalNovanda <cocwar427@gmail.com>
2023-09-26 10:24:32 +0700

    restructuring Folder

---
+++ Backend/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const mysql = require('mysql')
+const cors = require('cors')
+
+
+const app = express()
+app.use(cors())
+
+
+const db = mysql.createConnection({
+   host : "localhost",
+   user : "root",
+   password : "",
+   database : "tourism_destination"
+})
+
+app.get('/', (re, res)=> {
+   return res.json('From server side!')
+})
+
+
+app.get('/api/destination', (req, res)=>{
+   const query_sql = "SELECT * FROM destination_table"
+   db.query(query_sql, (err, data)=>{
+      if(err){ 
+         return res.json(err)
+      }
+      return res.json(data)
+   })
+})
+
+
+app.listen(8081, ()=>{
+   console.log('ready to listening')
+})
